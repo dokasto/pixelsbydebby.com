23edbd7a53673415309cb5a843c26c1ecf301730
creativecoderjoy <154061032+creativecoderjoy@users.noreply.github.com>
2024-07-23 18:36:14 +0100

    build

--- deploy.bat
+++ deploy.bat
@@ -1,6 +1,7 @@
 @echo off
 REM Navigate to the project directory (optional, if needed)
 npm run build && ^
+echo pixelsbydebby.com > docs\CNAME && ^
 git add . && ^
 git commit -m "build" && ^
 git pull && ^
